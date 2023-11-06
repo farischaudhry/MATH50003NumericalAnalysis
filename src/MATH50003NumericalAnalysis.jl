20de44310d1bfc7d72754f2ad8a67d2c72663903
Sheehan Olver <solver@mac.com>
2023-11-06 08:26:01 +0000

    Start notes

--- src/MATH50003NumericalAnalysis.jl
+++ src/MATH50003NumericalAnalysis.jl
@@ -1,6 +1,29 @@
 #####
+# notes
+#####
+
+using Weave
+
+
+function replacetheorem(path, thrm, Thrm)
+    str = read(path, String)
+    str = replace(str, Regex("\\\\textbf{$Thrm \\(([^}]*)\\)}(.*?)\\\\textbf{Proof}", "s") => SubstitutionString("\\\\begin{$thrm}[\\1]\\2\\\\end{$thrm}\\n\\\\textbf{Proof}"))
+    write(path, str)
+end
+function compilenotes(filename)
+    weave("src/notes/$filename.jmd"; out_path="notes/", doctype="md2tex", template="src/notes/template.tpl")
+    path = "notes/$filename.tex"
+    replacetheorem(path, "theorem", "Theorem")
+    replacetheorem(path, "lemma", "Lemma")
+    # work around double newline before equation
+    write(path, replace(read(path, String), "\n\n\\[" => "\n\\["))
+end
+
+compilenotes("I.1.Integration")
+
+#####
 # labs
-####
+#####
 
 import Literate
 
