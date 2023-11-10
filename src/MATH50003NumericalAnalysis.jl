8edba14c5cf38692fd998b9e583c926912da16bf
Sheehan Olver <solver@mac.com>
2023-11-10 15:45:41 +0000

    Add Sheet 1

--- src/MATH50003NumericalAnalysis.jl
+++ src/MATH50003NumericalAnalysis.jl
@@ -34,6 +34,27 @@ end
 compilenotes("I.1.RectangularRule")
 compilenotes("I.2.DividedDifferences")
 
+
+###
+# sheets
+###
+
+function compilesheet(filename)
+    write("sheets/$filename.jmd", replace(read("src/sheets/$(filename)s.jmd", String), r"\*\*SOLUTION\*\*(.*?)\*\*END\*\*"s => ""))
+    weave("sheets/$filename.jmd"; out_path="sheets/", doctype="md2tex", template="src/sheets/template.tpl")
+    path = "sheets/$filename.tex"
+    # work around double newline before equation
+    write(path, replace(read(path, String), "\n\n\\[" => "\n\\["))
+    # work around meeq 
+    write(path, replace(read(path, String), r"\\\[\n\\meeq\{(.*?)\}\n\\\]"s => s"\\meeq{\1}"))
+end
+
+compilesheet("sheet1")
+
+# notebook("sheets/sheet1.jmd"; pkwds...)
+# notebook("src/sheets/sheet1s.jmd"; pkwds...)
+
+
 #####
 # labs
 #####
