cfc2cc333107d589ac55279d9b4be7bf80d44228
Sheehan Olver <solver@mac.com>
2023-11-10 15:13:03 +0000

    reorg

--- src/MATH50003NumericalAnalysis.jl
+++ src/MATH50003NumericalAnalysis.jl
@@ -10,12 +10,21 @@ function replacetheorem(path, thrm, Thrm)
     str = replace(str, Regex("\\\\textbf{$Thrm \\(([^}]*)\\)}(.*?)\\\\textbf{Proof}", "s") => SubstitutionString("\\\\begin{$thrm}[\\1]\\2\\\\end{$thrm}\\n\\\\textbf{Proof}"))
     write(path, str)
 end
+
+function replacedefinition(path, thrm, Thrm)
+    str = read(path, String)
+    str = replace(str, Regex("\\\\textbf{$Thrm \\(([^}]*)\\)}(.*?)\\\\ensuremath{\\\\QED}", "s") => SubstitutionString("\\\\begin{$thrm}[\\1]\\2\\\\end{$thrm}"))
+    write(path, str)
+end
+
 function compilenotes(filename)
     weave("src/notes/$filename.jmd"; out_path="notes/", doctype="md2tex", template="src/notes/template.tpl")
     path = "notes/$filename.tex"
     replacetheorem(path, "theorem", "Theorem")
     replacetheorem(path, "lemma", "Lemma")
     replacetheorem(path, "proposition", "Proposition")
+    replacedefinition(path, "example", "Example")
+    replacedefinition(path, "definition", "Definition")
     # work around double newline before equation
     write(path, replace(read(path, String), "\n\n\\[" => "\n\\["))
     # work around meeq 
@@ -31,6 +40,13 @@ compilenotes("I.2.DividedDifferences")
 
 import Literate
 
-write("src/labs/lab1.jl", replace(replace(read("src/labs/lab1s.jl", String), r"## SOLUTION(.*?)## END"s => "")))
-Literate.notebook("src/labs/lab1.jl", "labs/"; execute=false)
-Literate.notebook("src/labs/lab1s.jl", "labs/"; execute=false)
+# Make Labs
+for k = 1:1
+    write("labs/lab$k.jl", replace(replace(read("src/labs/lab$(k)s.jl", String), r"## SOLUTION(.*?)## END"s => "")))
+    Literate.notebook("labs/lab$k.jl", "labs/"; execute=false)
+end
+
+
+# Make Solutions
+# Literate.notebook("src/labs/lab1s.jl", "labs/"; execute=false)
+
